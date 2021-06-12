679759dc26f173428a193e41c8486dc853eeb67a
Mahesh <maheshsainni@gmail.com>
2021-06-12 18:06:32 +0530

    AccesControlHandler updated

--- src/Access/DonationEntityAccessControlHandler.php
+++ src/Access/DonationEntityAccessControlHandler.php
@@ -24,14 +24,14 @@ class DonationEntityAccessControlHandler extends EntityAccessControlHandler {
 
       case 'view':
 
-        if (!$entity->isPublished()) {
-          $permission = $this->checkOwn($entity, 'view unpublished', $account);
-          if (!empty($permission)) {
-            return AccessResult::allowed();
-          }
+        // if (!$entity->isPublished()) {
+        //   $permission = $this->checkOwn($entity, 'view unpublished', $account);
+        //   if (!empty($permission)) {
+        //     return AccessResult::allowed();
+        //   }
 
-          return AccessResult::allowedIfHasPermission($account, 'view unpublished donation entities');
-        }
+        //   return AccessResult::allowedIfHasPermission($account, 'view unpublished donation entities');
+        // }
 
         $permission = $this->checkOwn($entity, $operation, $account);
         if (!empty($permission)) {
@@ -79,33 +79,33 @@ class DonationEntityAccessControlHandler extends EntityAccessControlHandler {
    *   The permission string indicating it's allowed.
    */
   protected function checkOwn(EntityInterface $entity, $operation, AccountInterface $account) {
-    $status = $entity->isPublished();
-    $uid = $entity->getOwnerId();
+    // $status = $entity->isPublished();
+    // $uid = $entity->getOwnerId();
 
-    $is_own = $account->isAuthenticated() && $account->id() == $uid;
-    if (!$is_own) {
-      return;
-    }
+    // $is_own = $account->isAuthenticated() && $account->id() == $uid;
+    // if (!$is_own) {
+    //   return;
+    // }
 
     $bundle = $entity->bundle();
 
     $ops = [
       'create' => '%bundle add own %bundle entities',
-      'view unpublished' => '%bundle view own unpublished %bundle entities',
+      // 'view unpublished' => '%bundle view own unpublished %bundle entities',
       'view' => '%bundle view own entities',
       'update' => '%bundle edit own entities',
       'delete' => '%bundle delete own entities',
     ];
     $permission = strtr($ops[$operation], ['%bundle' => $bundle]);
 
-    if ($operation === 'view unpublished') {
-      if (!$status && $account->hasPermission($permission)) {
-        return $permission;
-      }
-      else {
-        return NULL;
-      }
-    }
+    // if ($operation === 'view unpublished') {
+    //   if (!$status && $account->hasPermission($permission)) {
+    //     return $permission;
+    //   }
+    //   else {
+    //     return NULL;
+    //   }
+    // }
     if ($account->hasPermission($permission)) {
       return $permission;
     }
